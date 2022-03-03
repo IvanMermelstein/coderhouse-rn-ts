692773e243b588449c81dfd4bb29604bd6359847
Iván Mermelstein <ivan.mermelstein@gmail.com>
2022-03-03 12:49:17 -0300

    New: textInput and add button

---
+++ styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		padding: 15,
+		backgroundColor: '#333745',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	textInput: {
+		borderColor: '#b18fcf',
+		borderBottomWidth: 1,
+		marginBottom: 10,
+		width: '70%',
+		height: 40,
+		fontSize: 14,
+		color: '#978897'
+	}
+})
